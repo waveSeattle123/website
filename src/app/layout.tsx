d984e54ef021219ff593eb6fcb2dc6cece0c3073
datboi_212 <datboi212mc@gmail.com>
2025-04-15 12:24:03 -0700

    Font import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Head } from "next/document";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Limelight&family=Gloria+Hallelujah&display=swap'
+          rel='stylesheet'
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
