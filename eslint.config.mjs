8fcda6a27ff32b79adaf84661efdedaaaf7ec3b4
datboi_212 <datboi212mc@gmail.com>
2025-04-15 11:47:46 -0700

    Removed the ESLint unsecaped entity parsing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"; import { fileURLToPath } from "url"; import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url); const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname, });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = [ ...compat.extends("next/core-web-vitals", "next/typescript"), { rules: { "react/no-unescaped-entities": "off" } } ];
 
 export default eslintConfig;
